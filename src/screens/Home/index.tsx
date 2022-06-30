829b860421b766711137ed6316f4a9bfacc9b6b8
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2022-06-30 14:25:13 -0300

    t

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,35 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { FailedRequest } from '../../infrastructure/models/FailedRequests';
+import { RealmContext } from '../../infrastructure/realm';
 
 export default function HomeScreen() {
+  const { useQuery, useRealm } = RealmContext;
+  const realm = useRealm();
+  const failedRequest = useQuery(FailedRequest);
+
+  // useEffect(() => {
+  // }, []);
+
+  const handlePress = () => {
+    console.log('pressed');
+    realm.write(() => {
+      realm.create(
+        'FailedRequest',
+        FailedRequest.generate(
+          'https://www.npmjs.com/package/@realm/react',
+          JSON.stringify({ name: 'Jonathan', surname: 'Ferreira' })
+        )
+      );
+    });
+  };
+
   return (
     <View>
       <StatusBar style="auto" />
       <Text>Home Screen</Text>
-      <TouchableOpacity>Hello</TouchableOpacity>
+      <Button title="press mse" onPress={handlePress} />
     </View>
   );
 }
--- src/screens/Template/index.tsx
+++ src/screens/Template/index.tsx
@@ -1,6 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
 export default function TemplateScreen() {
   return (
     <View>
